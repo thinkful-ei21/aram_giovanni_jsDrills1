a0b3c768ba94739e472eb709858d27b56a20dae7
Aram Hammoudeh <aramcontact@gmail.com>
2018-05-07 12:27:38 -0600

    Created a new function

--- jsDrills1.js
+++ jsDrills1.js
@@ -1,8 +1,12 @@
 'use strict';
 
-function whoAmI(name = 'Chris', age = '29') {
+function whoAmI(name = 'Chris', age = 29) {
     console.log(`My name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in `, 2018-age);
+    console.log(`I was born in `, yearOfBirth(age));
+}
+
+function yearOfBirth(age = 29) {
+    return 2018 - age;
 }
 
 whoAmI();
