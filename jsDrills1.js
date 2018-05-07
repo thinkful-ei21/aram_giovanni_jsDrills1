4cc4e1ffac8a048213b44f416b481ae181bf475e
giovanni <mormagli@gmail.com>
2018-05-07 13:40:42 -0500

    in progress...

--- jsDrills1.js
+++ jsDrills1.js
@@ -2,11 +2,17 @@
 
 function whoAmI(name = 'Chris', age = 29) {
     console.log(`My name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in `, yearOfBirth(age));
+    let yob;
+    try{yob = yearOfBirth(age)}
+    catch(err){console.log(err)}
+    console.log(`I was born in ${yob}`);
 }
 
 function yearOfBirth(age = 29) {
-    return 2018 - age;
-}
+    if(age < 0){throw(`age can't be less than 0`)}
+    else{return 2018 - age;}
+    }
+    
 
-whoAmI();
+
+whoAmI('bob', -123);
