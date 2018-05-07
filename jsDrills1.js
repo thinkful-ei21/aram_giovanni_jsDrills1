9009a9f687d5a1e96afee4c6f5c53cc5025c77f5
Aram Hammoudeh <aramcontact@gmail.com>
2018-05-07 12:47:04 -0600

    Sorted errors

--- jsDrills1.js
+++ jsDrills1.js
@@ -1,18 +1,28 @@
 'use strict';
 
-function whoAmI(name = 'Chris', age = 29) {
-    console.log(`My name is ${name} and I'm ${age} years old`);
-    let yob;
-    try{yob = yearOfBirth(age)}
-    catch(err){console.log(err)}
-    console.log(`I was born in ${yob}`);
+function whoAmI(name, age) {
+    if (name === undefined || age === NaN) {
+        console.error("Argument is invalid")
+    } else {
+        console.log(`My name is ${name} and I'm ${age} years old`);
+        let yob;
+        try {
+            yob = yearOfBirth(age);
+            console.log(`I was born in ${yob}`);
+        } catch (err) {
+            console.log(err)
+        }
+    }
 }
 
-function yearOfBirth(age = 29) {
-    if(age < 0){throw(`age can't be less than 0`)}
-    else{return 2018 - age;}
+function yearOfBirth(age) {
+    if (age < 0) {
+        throw (`age can't be less than 0`)
+    } else {
+        return 2018 - age;
     }
-    
+}
 
 
-whoAmI('bob', -123);
+
+whoAmI("Johnny", 108);
