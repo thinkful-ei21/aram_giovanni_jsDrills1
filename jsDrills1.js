9fa445cb53442b1b17f214d0e2a8b1c28ea62930
giovanni <mormagli@gmail.com>
2018-05-07 14:15:49 -0500

    To infinity...

--- jsDrills1.js
+++ jsDrills1.js
@@ -25,4 +25,4 @@ function yearOfBirth(age) {
 
 
 
-whoAmI("Johnny");
+whoAmI();
--- jsDrills2.js
+++ jsDrills2.js
@@ -2,4 +2,12 @@ function jediName(firstName, lastName) {
     return (lastName.slice(0, 3)) + (firstName.slice(0, 2));
 }
 
-console.log(jediName("Test", "Dummy"));
+//console.log(jediName("Test", "Dummy"));
+
+function beyond(num){
+    if(Number.isFinite(num)){console.log('And beyond')}
+    else if(num > 0){console.log('To infinity')}
+    else if(num < 0){console.log('To negative infinity')}
+    else if(num === 0){console.log('Staying home')}
+
+}
